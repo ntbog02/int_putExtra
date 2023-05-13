046c3c68132514167a5bad23f62616065994297a
Natasha Bogado <b.ntasha14@gmail.com>
2023-05-13 15:37:00 -0300

    them.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = 0;
+        resultado = suma(4, 2);
+        System.out.println(resultado);
+
+    }
+    public static int suma(int a, int b){
+        return a + b;
+    }
+}
